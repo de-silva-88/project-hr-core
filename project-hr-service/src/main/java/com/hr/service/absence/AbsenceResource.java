8095f26916f4dc7ccb0a99e2bc7120a1bd1ace5b
duminda-skd <snjaya89@gmail.com>
2016-12-23 08:21:03 +0530

    WIP : jooq

--- project-hr-service/src/main/java/com/hr/service/absence/AbsenceResource.java
+++ project-hr-service/src/main/java/com/hr/service/absence/AbsenceResource.java
@@ -13,16 +13,36 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Path("/absence")
 public class AbsenceResource {
-	
-	private AbsenceController controller = new AbsenceController();
-	
-        @GET
-	@Path("/{id}")
-        @Produces(MediaType.APPLICATION_JSON)
-	public Response getLeavesLeftById(@PathParam("id") String idStr) {
-		log.info("Invoked get-leaves-left-by-id service with param --> id : {}", idStr);
-		int id = Integer.parseInt(idStr);
-		Employee emp = controller.getLeavesLeftForEmployee(id);
-		return Response.status(Response.Status.OK).entity(emp).build();
-	}
+
+    private AbsenceController controller = new AbsenceController();
+
+    @GET
+    @Path("/leaves/left/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getLeavesLeftByEmpId(@PathParam("id") String idStr) {
+        log.info("Invoked get-leaves-left-by-id service with param --> id : {}", idStr);
+        int id = Integer.parseInt(idStr);
+        Employee emp = controller.getLeavesLeftForEmployee(id);
+        return Response.status(Response.Status.OK).entity(emp).build();
+    }
+
+    @GET
+    @Path("/leaves/applied/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getLeavesAppliedByEmpId(@PathParam("id") String idStr) {
+        log.info("Invoked get-leaves-left-by-id service with param --> id : {}", idStr);
+        int id = Integer.parseInt(idStr);
+        Employee emp = controller.getLeavesLeftForEmployee(id);
+        return Response.status(Response.Status.OK).entity(emp).build();
+    }
+
+    @GET
+    @Path("/leaves/taken/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTotalLeavesTakenEmpId(@PathParam("id") String idStr) {
+        log.info("Invoked get-leaves-left-by-id service with param --> id : {}", idStr);
+        int id = Integer.parseInt(idStr);
+        Employee emp = controller.getLeavesLeftForEmployee(id);
+        return Response.status(Response.Status.OK).entity(emp).build();
+    }
 }
